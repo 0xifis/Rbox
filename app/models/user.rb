699cafa901060847c32b90032ed248e3396ff490
Vishnu Menon <vishthemenon@gmail.com>
2012-07-01 23:53:07 +0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, . :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :firstname, :lastname, :username, :email, :password, :password_confirmation, :remember_me
+  
+  validates_presence_of :firstname, :lastname, :username, :email, :password
+  validates_uniqueness_of :username, :email
+  validates_format_of :email, :with => /regex/i
+  validates_length_of :password, :minimum => 5, :message => "Sorry, your password is too short."
+  validates_length_of :username, :in => 5..15, :message => "Sorry, your username has to be 5-20 characters long." 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rbox::Application.config.secret_token = '04f40e4f692d274c6a9b45d7383869e8983f0d63e60b5d94114b31ad3adf045222699dc10fabc5391ad7d6f30dd9e36129695b8f5a78819e153685bbc8d193bf'
